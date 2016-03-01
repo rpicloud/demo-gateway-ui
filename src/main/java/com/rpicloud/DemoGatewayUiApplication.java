55d32775ea1c88ec8c46cf6a2fa126f39c55fc99
Kasper Nissen <kni@it-minds.dk>
2016-03-01 09:58:15 +0100

    Initial commit

---
+++ src/main/java/com/rpicloud/DemoGatewayUiApplication.java
@@ -0,0 +1,12 @@
+package com.rpicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGatewayUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGatewayUiApplication.class, args);
+	}
+}
